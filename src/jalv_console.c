8cde4e9fb99ace3a6f272805f53db8024568ce7d
jofemodo <fernando@zynthian.org>
2018-09-26 16:05:43 +0200

    Remove some unused variables from JSON output.

--- src/jalv_console.c
+++ src/jalv_console.c
@@ -168,7 +168,7 @@ jalv_print_control_info(Jalv* jalv)
 				printf("{ \"label\": \"%s\", \"value\": %f }",control->points[j].label,control->points[j].value);
 				if (j<(control->n_points-1)) printf(",");
 			}
-			printf(" ], \"min\": %f, \"max\": %f, \"default\": %f, \"value\": %f, \"is_toggle\": %d, \"is_integer\": %d, \"is_enumeration\": %d, \"is_logarithmic\": %d, \"is_writable\": %d, \"is_readable\": %d }\n",
+			printf(" ], \"min\": %f, \"max\": %f, \"default\": %f, \"value\": %f, \"is_toggle\": %d, \"is_integer\": %d, \"is_enumeration\": %d, \"is_logarithmic\": %d }\n",
 				lilv_node_as_float(control->min),
 				lilv_node_as_float(control->max),
 				lilv_node_as_float(control->def),
