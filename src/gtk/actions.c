7150309956f990f197840f8163b0e5ee357a423d
David Robillard <d@drobilla.net>
2026-06-03 13:03:26 -0400

    Improve save preset dialog

--- src/gtk/actions.c
+++ src/gtk/actions.c
@@ -12,13 +12,17 @@
 #include "../state.h"
 #include "../types.h"
 
+#include <glib-object.h>
+#include <gobject/gclosure.h>
 #include <gtk/gtk.h>
 #include <lilv/lilv.h>
 #include <zix/allocator.h>
 #include <zix/attributes.h>
+#include <zix/filesystem.h>
 #include <zix/path.h>
 #include <zix/string_view.h>
 
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -160,6 +164,70 @@ action_save_preset(GSimpleAction* const ZIX_UNUSED(action),
   lilv_free(path);
 }
 
+typedef struct {
+  Jalv*      jalv;
+  GString*   bundle_path;
+  GtkWidget* save_button;
+  GtkWidget* directory_button;
+  GtkWidget* name_label;
+  GtkWidget* name_entry;
+  GtkWidget* overwrite_box;
+  GtkWidget* overwrite_check_button;
+  GtkWidget* overwrite_label;
+} SavePresetWidgets;
+
+static void
+save_preset_widget_changed(const void* const ZIX_UNUSED(widget),
+                           gpointer          user_data)
+{
+  SavePresetWidgets* const widgets = (SavePresetWidgets*)user_data;
+  Jalv* const              jalv    = widgets->jalv;
+  const char* const        home    = g_get_home_dir();
+  const char* const        preset_name =
+    gtk_entry_get_text(GTK_ENTRY(widgets->name_entry));
+
+  if (!strlen(preset_name)) {
+    g_string_assign(widgets->bundle_path, "");
+    gtk_widget_set_sensitive(widgets->save_button, FALSE);
+    return;
+  }
+
+  const char* const plugin_name = lilv_node_as_string(jalv->plugin_name);
+
+  char* const long_name = g_strjoin(NULL, plugin_name, "_", preset_name, NULL);
+  char* const base      = symbolify(long_name);
+  char* const name      = g_strjoin(NULL, base, ".preset.lv2", NULL);
+  char* const path      = g_build_filename(home, ".lv2", name, NULL);
+
+  g_string_assign(widgets->bundle_path, (const char*)path);
+
+  GtkToggleButton* const overwrite_toggle =
+    GTK_TOGGLE_BUTTON(widgets->overwrite_check_button);
+
+  const bool file_exists = (zix_file_type(path) != ZIX_FILE_TYPE_NONE);
+  const bool overwrite   = gtk_toggle_button_get_active(overwrite_toggle);
+  if (!file_exists) {
+    gtk_label_set_markup(GTK_LABEL(widgets->overwrite_label), "");
+  } else {
+    LilvNode*   bundle_uri = lilv_new_file_uri(jalv->world, NULL, path);
+    char* const message    = g_strdup_printf(
+      "<a href=\"%s\">%s</a>", lilv_node_as_string(bundle_uri), base);
+
+    gtk_label_set_markup(GTK_LABEL(widgets->overwrite_label), message);
+
+    g_free(message);
+    lilv_node_free(bundle_uri);
+  }
+
+  gtk_widget_set_sensitive(widgets->save_button, !file_exists || overwrite);
+  gtk_widget_set_visible(widgets->overwrite_box, file_exists);
+
+  g_free(path);
+  g_free(name);
+  free(base);
+  g_free(long_name);
+}
+
 void
 action_save_preset_as(GSimpleAction* const ZIX_UNUSED(action),
                       GVariant* const      ZIX_UNUSED(parameter),
@@ -168,75 +236,109 @@ action_save_preset_as(GSimpleAction* const ZIX_UNUSED(action),
   Jalv* const jalv = (Jalv*)data;
   App* const  app  = (App*)jalv->app;
 
-  GtkWidget* dialog = gtk_file_chooser_dialog_new("Save Preset",
-                                                  app->window,
-                                                  GTK_FILE_CHOOSER_ACTION_SAVE,
-                                                  "_Cancel",
-                                                  GTK_RESPONSE_REJECT,
-                                                  "_Save",
-                                                  GTK_RESPONSE_ACCEPT,
-                                                  NULL);
+  GtkWidget* const dialog = gtk_dialog_new_with_buttons(
+    "Save Preset",
+    app->window,
+    (GtkDialogFlags)(GTK_DIALOG_USE_HEADER_BAR | GTK_DIALOG_MODAL),
+    "_Cancel",
+    GTK_RESPONSE_CANCEL,
+    "_OK",
+    GTK_RESPONSE_ACCEPT,
+    NULL);
 
-  char* dot_lv2 = g_build_filename(g_get_home_dir(), ".lv2", NULL);
-  gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(dialog), dot_lv2);
-  g_free(dot_lv2);
+  GtkWidget* const  content = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
+  GtkGrid* const    grid    = GTK_GRID(gtk_grid_new());
+  SavePresetWidgets widgets = {
+    jalv,
+    g_string_new(""),
+    gtk_dialog_get_widget_for_response(GTK_DIALOG(dialog), GTK_RESPONSE_ACCEPT),
+    gtk_file_chooser_button_new("Select an LV2 bundle _directory",
+                                GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER),
+    gtk_label_new("Name: "),
+    gtk_entry_new(),
+    gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 4),
+    gtk_check_button_new_with_mnemonic("_Overwrite"),
+    gtk_label_new(""),
+  };
 
-  GtkWidget* content   = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
-  GtkBox*    box       = GTK_BOX(gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 8));
-  GtkWidget* uri_label = gtk_label_new("URI (Optional):");
-  GtkWidget* uri_entry = gtk_entry_new();
-  GtkWidget* add_prefix =
-    gtk_check_button_new_with_mnemonic("_Prefix plugin name");
+  // Set save button as default and have name entry activate it
+  gtk_widget_set_sensitive(widgets.save_button, FALSE);
+  gtk_window_set_default(GTK_WINDOW(dialog), widgets.save_button);
+  gtk_entry_set_activates_default(GTK_ENTRY(widgets.name_entry), TRUE);
+  gtk_widget_grab_default(widgets.save_button);
 
-  gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(add_prefix), TRUE);
-  gtk_box_pack_start(box, uri_label, FALSE, TRUE, 2);
-  gtk_box_pack_start(box, uri_entry, TRUE, TRUE, 2);
-  gtk_box_pack_start(GTK_BOX(content), GTK_WIDGET(box), FALSE, FALSE, 6);
-  gtk_box_pack_start(GTK_BOX(content), add_prefix, FALSE, FALSE, 6);
+  g_signal_connect(G_OBJECT(widgets.name_entry),
+                   "changed",
+                   G_CALLBACK(save_preset_widget_changed),
+                   &widgets);
 
-  gtk_widget_show_all(GTK_WIDGET(dialog));
-  gtk_entry_set_activates_default(GTK_ENTRY(uri_entry), TRUE);
-  gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_ACCEPT);
+  g_signal_connect(G_OBJECT(widgets.overwrite_check_button),
+                   "toggled",
+                   G_CALLBACK(save_preset_widget_changed),
+                   &widgets);
+
+  // Configure grid to fill window horizontally (with some margin)
+  gtk_grid_set_row_spacing(GTK_GRID(grid), 8);
+  gtk_widget_set_margin_start(GTK_WIDGET(grid), 16);
+  gtk_widget_set_margin_end(GTK_WIDGET(grid), 16);
+  gtk_widget_set_margin_top(GTK_WIDGET(grid), 16);
+  gtk_widget_set_margin_bottom(GTK_WIDGET(grid), 16);
+  gtk_widget_set_halign(GTK_WIDGET(grid), GTK_ALIGN_FILL);
+  gtk_widget_set_valign(GTK_WIDGET(grid), GTK_ALIGN_START);
+  gtk_widget_set_hexpand(GTK_WIDGET(grid), TRUE);
+  gtk_widget_set_vexpand(GTK_WIDGET(grid), FALSE);
+
+  // Configure layout of content area widgets
+  gtk_widget_set_halign(widgets.name_label, GTK_ALIGN_END);
+  gtk_widget_set_halign(widgets.name_entry, GTK_ALIGN_FILL);
+  gtk_widget_set_hexpand(widgets.name_entry, TRUE);
+  gtk_widget_set_halign(widgets.overwrite_label, GTK_ALIGN_START);
+  gtk_widget_set_halign(widgets.overwrite_check_button, GTK_ALIGN_END);
+  gtk_box_pack_start(GTK_BOX(widgets.overwrite_box),
+                     widgets.overwrite_check_button,
+                     TRUE,
+                     TRUE,
+                     0);
+  gtk_box_pack_start(
+    GTK_BOX(widgets.overwrite_box), widgets.overwrite_label, TRUE, TRUE, 0);
+
+  // Attach widgets to grid
+  gtk_grid_attach(grid, widgets.name_label, 0, 0, 1, 1);
+  gtk_grid_attach(grid, widgets.name_entry, 1, 0, 1, 1);
+  gtk_grid_attach(grid, widgets.overwrite_box, 0, 1, 2, 1);
+
+  // Pack grid into content area and show initially visible contents
+  gtk_box_pack_start(GTK_BOX(content), GTK_WIDGET(grid), FALSE, FALSE, 8);
+  gtk_widget_show_all(dialog);
+  gtk_widget_hide(widgets.overwrite_box);
+
+  // Run the dialog and save the preset if the user accepted
   if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
-    const char* path = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
-    const char* uri  = gtk_entry_get_text(GTK_ENTRY(uri_entry));
-    const char* prefix = "";
-    const char* sep    = "";
-    if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(add_prefix))) {
-      prefix = lilv_node_as_string(jalv->plugin_name);
-      sep    = "_";
+    const char* const bundle = widgets.bundle_path->str;
+    const char* const label = gtk_entry_get_text(GTK_ENTRY(widgets.name_entry));
+
+    char* const base     = symbolify(label);
+    char* const filename = g_strjoin(NULL, base, ".ttl", NULL);
+    char* const dir      = zix_path_join(NULL, bundle, "");
+    if (!jalv_save_preset(jalv, dir, NULL, label, filename)) {
+      // (Re)load bundle into the world
+      LilvNode* ldir = lilv_new_file_uri(jalv->world, NULL, dir);
+      lilv_world_unload_bundle(jalv->world, ldir);
+      lilv_world_load_bundle(jalv->world, ldir);
+      lilv_node_free(ldir);
+
+      // Update UI
+      rebuild_preset_menu(jalv);
+      update_window(jalv);
     }
 
-    char* dirname  = g_path_get_dirname(path);
-    char* basename = g_path_get_basename(path);
-    char* sym      = symbolify(basename);
-    char* sprefix  = symbolify(prefix);
-    char* bundle   = g_strjoin(NULL, sprefix, sep, sym, ".preset.lv2/", NULL);
-    char* file     = g_strjoin(NULL, sym, ".ttl", NULL);
-    char* dir      = g_build_filename(dirname, bundle, NULL);
-
-    jalv_save_preset(jalv, dir, (strlen(uri) ? uri : NULL), basename, file);
-
-    // Reload bundle into the world
-    LilvNode* ldir = lilv_new_file_uri(jalv->world, NULL, dir);
-    lilv_world_unload_bundle(jalv->world, ldir);
-    lilv_world_load_bundle(jalv->world, ldir);
-    lilv_node_free(ldir);
-
-    // Update UI
-    rebuild_preset_menu(jalv);
-    update_window(jalv);
-
-    g_free(dir);
-    g_free(file);
-    g_free(bundle);
-    free(sprefix);
-    free(sym);
-    g_free(basename);
-    g_free(dirname);
+    zix_free(NULL, dir);
+    g_free(filename);
+    free(base);
   }
 
-  gtk_widget_destroy(GTK_WIDGET(dialog));
+  g_string_free(widgets.bundle_path, TRUE);
+  gtk_widget_destroy(dialog);
 }
 
 void
